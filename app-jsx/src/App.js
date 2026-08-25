508df1532400c6398762e0a8d92914bd0fe1b139
Sneider-09 <duvansneidercb@gmail.com>
2026-08-25 10:16:55 -0500

    JSX II. Manejo de eventos

--- app-jsx/src/App.js
+++ app-jsx/src/App.js
@@ -1,13 +1,32 @@
 import "./App.css";
+import React, { useState } from "react";
 
 function App() {
+  // Definir el estado para almacenar el resultado de la Suma
+  const [resultado, setResultado] = useState(null);
+
   const elemento = <h1 className="centrar-titulo">Hola Mundo</h1>;
-  const elemento2 = suma(5, 8);
+  /*const elemento2 = (
+    <h2>
+      {suma(25, 8)} 
+    </h2>
+  )*/
+
+  const botonPulsado = () => {
+    const result = suma(25, 8);
+    setResultado(result);
+  };
 
   return (
     <div>
+      <button
+        onClick={botonPulsado}
+        style={{ marginTop: "10px", marginLeft: "10px" }}
+      >
+        Púlsame
+      </button>
       <div>{elemento}</div>
-      <div>{elemento2}</div>
+      <div>{resultado != null && <h2>El resultado es: {resultado}</h2>}</div>
     </div>
   );
 }
@@ -15,5 +34,4 @@ function App() {
 function suma(a, b) {
   return a + b;
 }
-
 export default App;
