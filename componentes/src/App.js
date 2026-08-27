9bd203293bd60d5632b137780dc161d56143d0fc
Sneider-09 <duvansneidercb@gmail.com>
2026-08-27 08:42:26 -0500

    Componentes

---
+++ componentes/src/App.js
@@ -0,0 +1,16 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Contador from "./components/Contador";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <Contador />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ componentes/src/components/Contador.js
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+
+const Contador = () => {
+  const [cuenta, setCuenta] = useState(1);
+
+  const incrementar = () => {
+    setCuenta(cuenta + 1);
+  };
+
+  const disminuir = () => {
+    if (cuenta > 0) {
+      setCuenta(cuenta - 1);
+    }
+  };
+
+  return (
+    <div>
+      <button onClick={disminuir}>-</button>
+      <input
+        type="text"
+        value={cuenta}
+        size="2"
+        style={{ textAlign: "center" }}
+      ></input>
+      <button onClick={incrementar}>+</button>
+    </div>
+  );
+};
+
+export default Contador;
